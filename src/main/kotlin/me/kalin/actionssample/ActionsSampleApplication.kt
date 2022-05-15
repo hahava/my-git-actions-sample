c6483fc9ed7d6ac3b9d814d34c06617163780bab
hahava <hahava5696@gmail.com>
2022-05-15 13:38:33 +0900

    init

---
+++ src/main/kotlin/me/kalin/actionssample/ActionsSampleApplication.kt
@@ -0,0 +1,11 @@
+package me.kalin.actionssample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ActionsSampleApplication
+
+fun main(args: Array<String>) {
+    runApplication<ActionsSampleApplication>(*args)
+}
---
+++ src/test/kotlin/me/kalin/actionssample/ActionsSampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.kalin.actionssample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ActionsSampleApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
