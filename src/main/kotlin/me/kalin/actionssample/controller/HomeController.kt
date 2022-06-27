30b90bce6af951c52bc62b66dc4d26a60e581420
kalin-lee <kalin@dunamu.com>
2022-06-27 14:03:54 +0900

    Add scan-action

--- src/main/kotlin/me/kalin/actionssample/controller/HomeController.kt
+++ src/main/kotlin/me/kalin/actionssample/controller/HomeController.kt
@@ -1,11 +1,18 @@
 package me.kalin.actionssample.controller
 
+import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 import java.time.LocalDateTime
 
 @RestController
 class HomeController {
+
+    private val logger = LoggerFactory.getLogger(this.javaClass)
+
     @GetMapping("/v1/api/home")
-    fun getHome(): String = LocalDateTime.now().toString()
+    fun getHome(): String {
+        logger.info("hello")
+        return LocalDateTime.now().toString()
+    }
 }
